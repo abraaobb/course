9db99c5f23de1227c453f4b4c9604a3021bbec26
Abraao Brandao <abraao.brandao@fpf.br>
2022-08-30 10:27:23 -0400

    commit

--- core/viewsets.py
+++ core/viewsets.py
@@ -1,7 +1,8 @@
 from rest_framework import viewsets
+
 from core import models, serializers
 
 
-class CouseViewSet(viewsets.ModelViewSet):
+class CourseViewSet(viewsets.ModelViewSet):
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
