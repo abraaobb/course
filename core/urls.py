a6c0265917e03b56e75c738eff0b2ce62f0724e9
Abraao Brandao <abraao.brandao@fpf.br>
2022-09-02 22:52:18 -0400

    router

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,6 @@ from rest_framework.routers import DefaultRouter
 from core import viewsets
 
 router = DefaultRouter()
-router.register('course', viewsets.CouseViewSet)
+router.register('course', viewsets.CourseViewSet)
 
 urlpatterns = router.urls
